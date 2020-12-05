0be14dedd7092a6760c271fb72d99b79a496a565
Aileran <aileran6@gmail.com>
2020-12-05 17:44:56 -0500

    Initial commit

--- curator/models.py
+++ curator/models.py
@@ -1,11 +1,32 @@
 from django.db import models
 
-# Create your models here.
-#book entry for book form
+
 class Book(models.Model):
     title = models.CharField(max_length=60)
     author = models.CharField(max_length=60)
     publisher = models.CharField(max_length=60)
     date = models.DateField()
+    genre = models.CharField(max_length=60)
     #rating
     comment = models.TextField()
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=60)
+    date = models.DateField()
+    #rating
+    comment = models.TextField()
+    director = models.CharField(max_length=60)
+    studio = models.CharField(max_length=60)
+    genre = models.CharField(max_length=60)
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=60)
+    date = models.DateField()
+    #rating
+    comment = models.TextField()
+    artist = models.CharField(max_length=60)
+    genre = models.CharField(max_length=60)
+    label = models.CharField(max_length=60)
+
