563a72782a0580b23efa3318f61760bbd3de0049
Aileran <aileran6@gmail.com>
2020-12-06 15:16:38 -0500

    model updates

--- curator/models.py
+++ curator/models.py
@@ -5,28 +5,33 @@ class Book(models.Model):
     title = models.CharField(max_length=60)
     author = models.CharField(max_length=60)
     publisher = models.CharField(max_length=60)
-    date = models.DateField()
     genre = models.CharField(max_length=60)
+    year = models.CharField(max_length=4)
+    date_added = models.DateField(auto_now_add=True)
     #rating
     comment = models.TextField()
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=60)
-    date = models.DateField()
-    #rating
-    comment = models.TextField()
     director = models.CharField(max_length=60)
     studio = models.CharField(max_length=60)
     genre = models.CharField(max_length=60)
+    year = models.CharField(max_length=4)
+    date_added = models.DateField(auto_now_add=True)
+    #rating
+    comment = models.TextField()
 
 
 class Album(models.Model):
     title = models.CharField(max_length=60)
-    date = models.DateField()
-    #rating
     comment = models.TextField()
     artist = models.CharField(max_length=60)
-    genre = models.CharField(max_length=60)
     label = models.CharField(max_length=60)
+    genre = models.CharField(max_length=60)
+    year = models.CharField(max_length=4)
+    date_added = models.DateField(auto_now_add=True)
+    #rating
+    comment = models.TextField()
+
 
