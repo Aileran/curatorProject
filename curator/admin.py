0721233be26731c496d3e17ae4d6c8a4feaed647
Aileran <aileran6@gmail.com>
2020-12-06 18:46:51 -0500

    admin commit

--- curator/admin.py
+++ curator/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 #from .models import UserRegistrationForm
 # Register your models here.
 #admin.site.register(UserRegistrationForm)
+from .models import *
+
+admin.site.register(Book)
+admin.site.register(Movie)
+admin.site.register(Album)
+
