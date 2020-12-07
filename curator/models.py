d71d5f4d8327703ae5b2b227c6a4080085c5294e
Aileran <aileran6@gmail.com>
2020-12-07 14:21:10 -0500

    models changes

--- curator/models.py
+++ curator/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 #each is a model of one type of media: book, movie, or album
 class Book(models.Model):
@@ -10,6 +11,7 @@ class Book(models.Model):
     #date_added = models.DateField(auto_now_add=True)
     #rating = models.IntegerField(blank=True, choices=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     comment = models.TextField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
@@ -24,6 +26,7 @@ class Movie(models.Model):
     #date_added = models.DateField(auto_now_add=True)
     #rating = models.IntegerField(blank=True, choices=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     comment = models.TextField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
@@ -38,6 +41,7 @@ class Album(models.Model):
     #date_added = models.DateField(auto_now_add=True)
     #rating = models.IntegerField(blank=True, choices=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     comment = models.TextField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
