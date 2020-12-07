9381ea502fd95142c6c22d4aa7739319df6d79b9
Aileran <aileran6@gmail.com>
2020-12-07 00:42:22 -0500

    views changes

--- curator/forms.py
+++ curator/forms.py
@@ -4,18 +4,20 @@ from .models import *
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 #^^^^^^^
+
+#create forms for each collection, this one is for book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = '__all__'
 
-
+#create form for movie collection
 class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
         fields = '__all__'
 
-
+#create form for album collection
 class AlbumForm(forms.ModelForm):
     class Meta:
         model = Album
