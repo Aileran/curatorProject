dd4c28c295aa4c8f1edb07589961937ebea7886c
joshschilling <j.schilling@my.ccsu.edu>
2020-12-09 17:28:56 -0500

    .

---
+++ static/js/sound.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    var audioElement = document.createElement("audio");
+    audioElement.src = "https://raw.githubusercontent.com/Metastruct/garrysmod-chatsounds/master/sound/chatsounds/autoadd/snoop_dogg/hold%20up%20wait.ogg";
+    $('#Play_Button').click(function(){
+        audioElement.play();
+    });
+});
