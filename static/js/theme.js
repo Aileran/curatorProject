78f09c90d46595da47228843dccfd4f5fc50056e
joshschilling <j.schilling@my.ccsu.edu>
2020-12-09 22:40:22 -0500

    .

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
+function changeTheme(id){
+    document.body.style.backgroundImage="url('images/meme.jpg')"
+}
 
+function changeTheme2(id){
+    document.body.style.backgroundColor="black"
+}
+
+function changeTheme3(id){
+    document.body.style.backgroundColor="black"
 }
